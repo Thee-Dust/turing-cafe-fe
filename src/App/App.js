3243c2d30bea8daa2e8f05bde026ce95f9ac48dd
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-26 11:33:02 -0400

    Add form file

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from '../Form/Form'
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentDidMount = async() => {
+
+  }
+
+
   render() {
     return (
       <div className="App">
