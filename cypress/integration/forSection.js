5c27158d34390d262852e5041a8879a547ee35a9
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-26 13:52:34 -0400

    Fix test questions

--- cypress/integration/forSection.js
+++ cypress/integration/forSection.js
@@ -4,19 +4,19 @@ describe("Form", () => {
   })
 
   it('should be able to type into name section', () => {
-    cy.get('input[name=nameInput]').type('Dustin')
+    cy.get('input[name=nameInput]').type('Dustin').should('have.value','Dustin')
   })
 
   it('should be able to select date', () => {
-    cy.get('input[name=dateInput]').type('2021-04-26')
+    cy.get('input[name=dateInput]').type('2021-04-26').should('have.value','2021-04-26')
   })
 
   it('should be able to select a time', () => {
-    cy.get('input[name=timeInput]').type('11:30')
+    cy.get('input[name=timeInput]').type('11:30').should('have.value','11:30')
   })
 
   it('should be able to select number of guest', () => {
-    cy.get('input[name=guestInput]').type('4')
+    cy.get('input[name=guestInput]').type('4').should('have.value','4')
   })
 
   it('should be able to make a new reservation', () => {
